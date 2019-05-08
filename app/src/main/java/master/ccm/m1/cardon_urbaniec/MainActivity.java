f291b880c2cf29a7a56142955499fba28e4449b8
CARDON <cardonkevin02@gmail.com>
2019-05-08 23:34:21 +0200

    Ajout du service de téléchargement mono-url et gestion des permissions

--- app/src/main/java/master/ccm/m1/cardon_urbaniec/MainActivity.java
+++ app/src/main/java/master/ccm/m1/cardon_urbaniec/MainActivity.java
@@ -1,14 +1,133 @@
 package master.ccm.m1.cardon_urbaniec;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * Activité principale de l'application
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Liste des variables
+     */
+    private NotreServiceDeTelechargement notreServiceTelechargement;
+    private ProgressDialog mProgressDialog;
+    private EditText editText;
+    private ImageView mImageView;
+
+    /**
+     * Instantiation des données à la création du composant
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // affectation des objets de la vue
+        mImageView = findViewById(R.id.imageView_image_afficher);
+        editText = findViewById(R.id.editText_url_download);
+
+        // initialise l'objet pour notre loader (Progres Dialog)
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        // Affectation du titre
+        mProgressDialog.setTitle("AsyncTask");
+        // Affectation du message contenu
+        mProgressDialog.setMessage("Please wait, we are downloading your image file...");
+
+        // Lie le service
+        Intent intent = new Intent(this, NotreServiceDeTelechargement.class);
+        bindService(intent, maConnexion, Context.BIND_AUTO_CREATE);
+    }
+
+
+    /**
+     * Vérification des permissions au démarrage
+     * Permission d'écrire sur le stockage externe
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //Demande la permission si non trouvé
+
+            Log.d("PERMISSION_ANDROID", "Permission non attribuée dans l'environnement");
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+
+        } else {
+            Log.d("PERMISSION_ANDROID", "Permission déjà attribuée à l'application");
+        }
+    }
+
+    /**
+     * Instancie notre connexion à notre service
+     */
+    private ServiceConnection maConnexion = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            notreServiceTelechargement = ((NotreServiceDeTelechargement.NotreBinder) service).seConnecter(MainActivity.this);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            notreServiceTelechargement = null;
+        }
+    };
+
+    /**
+     * Début du téléchargemment, affichage du loader tant que le téléchargement n'est pas fini
+     */
+    public void displayProcessus() {
+        mProgressDialog.show();
+    }
+
+    /**
+     * Fin du téléchargement, sauvegarde de l'image et fin du loader
+     * @param result est un objet Bitmap
+     */
+    public void stopProcessus(Bitmap result) {
+        if (result != null) {
+            // Sauvegarde l'image dans le stockage
+            Uri imageInternalUri = this.notreServiceTelechargement.saveImageToInternalStorage(result);
+            // affecte l'image télécharger
+            mImageView.setImageURI(imageInternalUri);
+        } else {
+            // Notifie l'utilisateur en cas d'erreur de téléchargement
+            Toast.makeText(this, "Error with the download of your image", Toast.LENGTH_LONG).show();
+        }
+
+        // Cacher le loader
+        mProgressDialog.dismiss();
+    }
+
+    /**
+     * Demande le téléchargement au clic sur le bouton télécharger
+     * Utilise la valeur de l'url pour le téléchargement
+     * @param view est un objet View
+     */
+    public void DemanderTelechargement(View view) {
+        this.notreServiceTelechargement.executeUneTacheDeTelechargement(editText.getText().toString());
     }
 }
---
+++ app/src/main/java/master/ccm/m1/cardon_urbaniec/NotreServiceDeTelechargement.java
@@ -0,0 +1,180 @@
+package master.ccm.m1.cardon_urbaniec;
+
+import android.app.Service;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Classe de notre service de téléchargement
+ */
+public class NotreServiceDeTelechargement extends Service {
+    private NotreServiceDeTelechargement.NotreBinder notreBinder = new NotreServiceDeTelechargement.NotreBinder();
+    private MainActivity mainActivity;
+
+    /**
+     * Classe interne qui retourne la classe NotreServiceDeTelechargement
+     */
+    public class NotreBinder extends Binder {
+
+
+        public NotreServiceDeTelechargement seConnecter(MainActivity activity) {
+            mainActivity = activity;
+            return NotreServiceDeTelechargement.this;
+        }
+    }
+
+    /**
+     * Ecoute la connexion
+     * @param intent
+     * @return Ibinder pour la liaison avec la connexion de l'activité
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return notreBinder;
+    }
+
+    /**
+     * Lance la tâche de téléchargement asynchrone
+     * @param url est une chaine définissant l'url à de l'image à récupérer
+     */
+    public void executeUneTacheDeTelechargement(String url){
+        new NotreServiceDeTelechargement.DownloadTask().execute(stringToURL(url));
+    }
+
+    /**
+     * Permet la conversion de chaine de caracteres en URL
+     * @param urlString est la chaine à convertir
+     */
+    protected URL stringToURL(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            return url;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Permet la sauvegarde de l'image dans le stockage externe
+     * @param bitmap est l'objet nécessaire à la sauvegarde de l'image
+     */
+    protected Uri saveImageToInternalStorage(Bitmap bitmap){
+        // Récupération du contexte
+        ContextWrapper wrapper = new ContextWrapper(getApplicationContext());
+
+        // Création du fichier de l'image à sauvegarder
+        File file = new File(Environment.getExternalStorageDirectory(), "UniqueFileName"+".jpg");
+
+        try{
+            // Créer le flux de sortie du fichier
+            OutputStream stream = null;
+
+            // si le fichier existe, il est alors écrasé
+            stream = new FileOutputStream(file);
+
+            // Compression de l'objet Bitmap au format JPEG .jpg
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+
+            // Nettoyage de la diffusion
+            stream.flush();
+
+            // fermeture de la diffusion
+            stream.close();
+
+        }catch (IOException e)
+        {
+            // En cas d'erreur, l'affiché
+            e.printStackTrace();
+        }
+
+        // Parser l'url de la galerie d'image en uri
+        Uri savedImageURI = Uri.parse(file.getAbsolutePath());
+
+        // retourner la valeur de l'uri
+        return savedImageURI;
+    }
+
+    /**
+     * Classe interne s'occupant du téléchargement de l'image de façon asynchrone
+     */
+    private class DownloadTask extends AsyncTask<URL,Void,Bitmap> {
+
+        /**
+         * Avant l'éxécution de la tâche, demande à l'activité d'affiché le loader.
+         */
+        protected void onPreExecute(){
+            // Display the progress dialog on async task start
+            mainActivity.displayProcessus();
+        }
+
+        /**
+         * Télécharge en arrière plan, n'est pas fait dans le Thread de l'UI
+         */
+        protected Bitmap doInBackground(URL...urls){
+            URL url = urls[0];
+            HttpURLConnection connection = null;
+
+            try{
+                // Initialise une nouvelle connexion HTTP vers une url
+                connection = (HttpURLConnection) url.openConnection();
+
+                // connexion à l'url
+                connection.connect();
+
+                // Obtention du flux d'entrée à partir de la connexion http url
+                InputStream inputStream = connection.getInputStream();
+
+                /*
+                         BufferedInputStream ajoute des fonctionnalités à un autre flux d’entrée, à savoir: la possibilité de mettre en tampon l'entrée et de supporter les méthodes de marquage et de réinitialisation.
+                         Crée un BufferedInputStream et enregistre son argument, le flux d'entrée dans, pour une utilisation ultérieure.
+                */
+                // Initialise un nouveau BufferedInputStream à partir de InputStream
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+                /*
+                         Bitmap decodeStream permet de décoder un flux d'entrée en un bitmap. Si le flux d'entrée est nul ou ne peut pas être utilisé pour décoder un bitmap, la fonction renvoie null.
+                         Résultats: Bitmap: bitmap décodé, ou null si les données de l'image n'ont pas pu être décodées.
+                */
+                // Convertit un BufferedInputStream en un objet Bitmap
+                Bitmap bmp = BitmapFactory.decodeStream(bufferedInputStream);
+
+                // retourne le bitmap téléchargé
+                return bmp;
+
+            }catch(IOException e){
+                // affichage de l'erreur en cas d'erreur
+                e.printStackTrace();
+            }finally{
+                // déconnecte la requête http
+                connection.disconnect();
+            }
+            return null;
+        }
+
+        /**
+         * Quand l'éxécution de la tâche est fini, demande à l'activité de finir le processus du loader.
+         */
+        protected void onPostExecute(Bitmap result){
+            mainActivity.stopProcessus(result);
+        }
+
+    }
+}
