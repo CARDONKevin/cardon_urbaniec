f100e245170ca90d5b6b118672152a8df6e66833
Urbaniec Rémi <urbaniec.remi@gmail.com>
2019-06-02 02:05:57 +0200

    Alors j'ai pas réussi à corriger l'eurreur et j'ai trouver un autre bug. Donc premier erreur c'est quand mon met un lien et on click sur le bouton telecharger non grisé. Le deuxième bug c'est qu'il affiche dans la listview que le dernier dans le fichier alors qu'ii devraient en afficher plusieurs. Par ailleurs j'ai laissé des log pour que tu puisse plus facilement debugger n'oublie pas de les supprimer si tu veux envoyer le projet.

---
+++ app/src/main/java/master/ccm/m1/cardon_urbaniec/FileManager.java
@@ -0,0 +1,98 @@
+package master.ccm.m1.cardon_urbaniec;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class FileManager {
+
+    // on set le nom du fichier qui stokera nos url
+    private static String NAMEFILE ="listeUrl.txt";
+
+    //fonction permettant d'ecrire dans un fichier
+    public static void writeToFile(String data, Context context) {
+
+
+        try {
+            //on ouvre le fichier pour ecrire
+
+
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(NAMEFILE, Context.MODE_PRIVATE));
+            // on ajoute à la fin du fichier l'url en question
+            outputStreamWriter.write(data);
+            outputStreamWriter.append(";");
+            outputStreamWriter.close();
+
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+    //fonction de suppression des données dans le fichier
+    public static void writeErased( Context context) {
+        try {
+            //on ouvre le fichier pour ecrire
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(NAMEFILE, Context.MODE_PRIVATE));
+            //on fait un write pour remplacer le contenue par une chaine vide
+            outputStreamWriter.write("");
+            //on ferme le fichier
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+    //fonction permettant de lire un fichier
+    public static String[] readFromFile(Context context) {
+        String[] ret = null;
+        try {
+            InputStream inputStream = context.openFileInput(NAMEFILE);
+            if ( inputStream != null ) {
+                //on ouvre le fichier pour le lire
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString ;
+                String stringBuilder ="" ;
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    Log.i("Remi_LOG", "stringBuilder : " + receiveString);
+                    stringBuilder += receiveString;
+
+
+                }
+
+                ret=stringBuilder.split(";");
+                Log.i("Remi_LOG", "length ret : " + ret.length);
+                //bufferedReader.close();
+                inputStream.close();
+                /*
+
+                ArrayList<String> stringTabBuilder = new ArrayList<String>();
+
+                ret = new String[stringTabBuilder.size()];
+                int x = 0;
+                for (String uneChaine : stringTabBuilder)
+                {
+                    ret[x] = uneChaine;
+                    x++;
+                }*/
+
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+        return ret;
+    }
+}
--- app/src/main/java/master/ccm/m1/cardon_urbaniec/MainActivity.java
+++ app/src/main/java/master/ccm/m1/cardon_urbaniec/MainActivity.java
@@ -30,7 +30,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Activité principale de l'application
@@ -47,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView listViewImage;
     private Button boutonTelechargerListeChecked;
     private String[] tableauChaines;
+    private ArrayAdapter<String> monArrayAdapter;
     private int nombreATelecharger;
     private int nombreDejaTelecharger;
     private HashMap<String, ImageView> mapImageViewAppartenantUrl;
@@ -57,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //suppresion du fichier pour un nouveau
+        //FileManager.writeErased(this);
 
         mapImageViewAppartenantUrl = new HashMap<>();
         nombreATelecharger = 0;
@@ -82,12 +89,12 @@ public class MainActivity extends AppCompatActivity {
         listViewImage = findViewById(R.id.listViewImage);
 
         //récuperation des urls des image
-        tableauChaines = getResources().getStringArray(R.array.tableau_image);
-
+        //tableauChaines = getResources().getStringArray(R.array.tableau_image);
+        tableauChaines = FileManager.readFromFile(this);
         //adapter fais le lien entre la liste et le tableau d'image
-        ArrayAdapter<String> monArrayAdapter = new ArrayAdapter(this, R.layout.descripteur_liste_image, R.id.tv_url_image, tableauChaines);
+        //monArrayAdapter = new ArrayAdapter(this, R.layout.descripteur_liste_image, R.id.tv_url_image, tableauChaines);
         listViewImage.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        listViewImage.setAdapter(monArrayAdapter);
+        //listViewImage.setAdapter(monArrayAdapter);
 
         boutonTelechargerListeChecked = findViewById(R.id.id_btn_telecharger_list_view_items);
         boutonTelechargerListeChecked.setEnabled(false);
@@ -112,6 +119,26 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }});
+
+
+        // test ajout dans le fichier avant
+        FileManager.writeToFile("https://uploads2.yugioh.com/card_images/1787/detail/2785.jpg?1385102370",this);
+        FileManager.writeToFile("http://uploads1.yugioh.com/card_images/275/detail/Kuriboh.jpg?1375127846",this);
+        //récuperation des urls des image
+        //tableauChaines =  getResources().getStringArray(R.array.tableau_image);
+        tableauChaines = FileManager.readFromFile(this);
+        //adapter fais le lien entre la liste et le tableau d'image
+        if(tableauChaines.length != 0){
+            Log.i("Remi_LOG", "length : " + tableauChaines.length);
+            Log.i("Remi_LOG", "Valeurstr : " + tableauChaines[0] );
+            //Log.i("Remi_LOG", "Valeurstr : " + tableauChaines[0] + " / " + tableauChaines[1]);
+
+
+            monArrayAdapter = new ArrayAdapter<String>(this, R.layout.descripteur_liste_image, R.id.tv_url_image, tableauChaines);
+
+            listViewImage.setAdapter(monArrayAdapter);
+        }
+
     }
 
 
@@ -227,7 +254,9 @@ public class MainActivity extends AppCompatActivity {
      * @param view est un objet View
      */
     public void DemanderTelechargement(View view) {
+        sauvegardeFichier(editText.getText().toString());
         this.notreServiceTelechargement.executeUneTacheDeTelechargement(editText.getText().toString());
+
     }
 
     public void DemanderTelechargementItemListView(View view) {
@@ -289,5 +318,17 @@ public class MainActivity extends AppCompatActivity {
         bundle.putString("UrlDeMonImageFormatString", ((TextView)((ViewGroup) view).getChildAt(1)).getText().toString());
         intent.putExtras(bundle);
         startActivity(intent);
+    }
+    //sauvegarde les données dans un fichier text
+    public void sauvegardeFichier(String chaineUrl) {
+        FileManager.writeToFile(chaineUrl,this);
+        MJListeView();
+    }
+
+    //met à jour la listeView
+    public void MJListeView() {
+        tableauChaines = FileManager.readFromFile(this);
+        monArrayAdapter = new ArrayAdapter<String>(this, R.layout.descripteur_liste_image, R.id.tv_url_image, tableauChaines);
+        listViewImage.setAdapter(monArrayAdapter);
     }
 }
